a6d6dd58cdae24a712a41d2f4e2d30139ff7212e
gpt4 <gpt4@gpt4.gpt4>
2023-05-16 10:54:24 +0530

    apis in saparate file constants.js

--- src/service/constants.js
+++ src/service/constants.js
@@ -1,3 +1,11 @@
 const employerLoginApi = "";
 const jobSeekerLoginApi = "";
-const getAllJobsApi = "https://64571a7e5f9a4f236151cbaf.mockapi.io/api/jobs";
+const getAllJobsApi = "http://localhost:8020/jobservice/alljobs";
+// "https://64571a7e5f9a4f236151cbaf.mockapi.io/api/jobs";
+const signupApi = "http://localhost:7171/api/auth/signup";
+const signinApi = "http://localhost:7171/api/auth/signin";
+const newjobpostApi = "http://localhost:8020/jobservice/recruiter/jobpost";
+const deleteJobByIdApi =
+  "http://localhost:8020/jobservice/recruiter/deletejobpost/";
+const updateAJobPost =
+  "http://localhost:8020/jobservice/recruiter/updatejobpost/2";
