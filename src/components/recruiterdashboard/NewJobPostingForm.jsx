278b90925142b240baa7b7dde91205e77a383b93
abhishek kumar <flashtoak@gmail.com>
2023-06-09 13:48:31 +0530

    added response message in jobpost

--- src/components/recruiterdashboard/NewJobPostingForm.jsx
+++ src/components/recruiterdashboard/NewJobPostingForm.jsx
@@ -4,7 +4,6 @@ import styled from "styled-components";
 import { postNewJobAction } from "../../store/slices/recruiter/recruiternewjobslice";
 import { setCommentRange } from "typescript";
 
-
 const PostNewJob = () => {
   const [jobData, setJobData] = useState({
     jobTitle: "",
@@ -15,7 +14,7 @@ const PostNewJob = () => {
     salary: "",
     qualification: "",
     vacancies: "",
-    companyName: ""
+    companyName: "",
   });
   const [responseMessage, setResponseMessage] = useState(null);
   const dispatch = useDispatch();
@@ -28,12 +27,12 @@ const PostNewJob = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     const result = await dispatch(postNewJobAction(jobData));
-    if (result.message) {
-      setResponseMessage(result.message);
-    }
+
+    setResponseMessage("Job Posted Sucessfully");
+
     setTimeout(() => {
       setResponseMessage(null);
-    }, 2000)
+    }, 2000);
   };
   //   {
   //     "jobTitle":"Python full stack developer",-----
@@ -50,94 +49,93 @@ const PostNewJob = () => {
   // }
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Input
-        type="text"
-        name="jobTitle"
-        placeholder="Job Title"
-        onChange={handleInputChange}
-      />
-
-      <Input
-        type="text"
-        name="location"
-        placeholder="Job Location"
-        onChange={handleInputChange}
-      />
-      <Input
-        type="text"
-        name="jobType"
-        placeholder="Job Type"
-        onChange={handleInputChange}
-      />
-      <Input
-        type="text"
-        name="experience"
-        placeholder="Experience"
-        onChange={handleInputChange}
-      />
-      <Input
-        type="text"
-        name="salary"
-        placeholder="Salary"
-        onChange={handleInputChange}
-      />
-
-      <Input
-        type="text"
-        name="qualification"
-        placeholder="Qualification"
-        onChange={handleInputChange}
-      />
-
-      <Input
-        type="text"
-        name="vacancies"
-        placeholder="Vacancies"
-        onChange={handleInputChange}
-      />
-      <Input
-        type="text"
-        name="companyName"
-        placeholder="companyName"
-        onChange={handleInputChange}
-      />
-      <Input
-        type="text"
-        name="skillset"
-        placeholder="Skillset"
-        onChange={handleInputChange}
-      />
-
-      <TextArea style={{ gridColumnStart: 1, gridColumnEnd: 3 }}
-        type="text"
-        name="jobDescription"
-        placeholder="Job Description"
-        onChange={handleInputChange}
-      ></TextArea>
-
-
-
-      <Button type="submit" disabled={loading}>
-        POST JOB
-      </Button>
+    <>
       {responseMessage && <div>{responseMessage}</div>}
-    </Form>
+
+      <Form onSubmit={handleSubmit}>
+        <Input
+          type="text"
+          name="jobTitle"
+          placeholder="Job Title"
+          onChange={handleInputChange}
+        />
+
+        <Input
+          type="text"
+          name="location"
+          placeholder="Job Location"
+          onChange={handleInputChange}
+        />
+        <Input
+          type="text"
+          name="jobType"
+          placeholder="Job Type"
+          onChange={handleInputChange}
+        />
+        <Input
+          type="text"
+          name="experience"
+          placeholder="Experience"
+          onChange={handleInputChange}
+        />
+        <Input
+          type="text"
+          name="salary"
+          placeholder="Salary"
+          onChange={handleInputChange}
+        />
+
+        <Input
+          type="text"
+          name="qualification"
+          placeholder="Qualification"
+          onChange={handleInputChange}
+        />
+
+        <Input
+          type="text"
+          name="vacancies"
+          placeholder="Vacancies"
+          onChange={handleInputChange}
+        />
+        <Input
+          type="text"
+          name="companyName"
+          placeholder="companyName"
+          onChange={handleInputChange}
+        />
+        <Input
+          type="text"
+          name="skillset"
+          placeholder="Skillset"
+          onChange={handleInputChange}
+        />
+
+        <TextArea
+          style={{ gridColumnStart: 1, gridColumnEnd: 3 }}
+          type="text"
+          name="jobDescription"
+          placeholder="Job Description"
+          onChange={handleInputChange}
+        ></TextArea>
+        {responseMessage && <div>{responseMessage}</div>}
+        <Button type="submit" disabled={loading}>
+          POST JOB
+        </Button>
+      </Form>
+    </>
   );
 };
 
 export default PostNewJob;
 
-
 const Form = styled.form`
   display: flex;
-  flex-direction:column;
-  @media screen and (min-width:700px){
-    display:grid;
-    grid-template-columns:repeat(2,1fr);
-  
+  flex-direction: column;
+  @media screen and (min-width: 700px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
   }
-  
 `;
 
 const Input = styled.input`
@@ -154,9 +152,9 @@ const TextArea = styled.textarea`
   font-size: 16px;
   border: 1px solid #ccc;
   border-radius: 5px;
-  min-height:200px;
-  max-height:500px;
-  overflow:auto;
+  min-height: 200px;
+  max-height: 500px;
+  overflow: auto;
 `;
 
 const Button = styled.button`
@@ -168,9 +166,8 @@ const Button = styled.button`
   border: none;
   border-radius: 5px;
   cursor: pointer;
-  width:100px;
+  width: 100px;
   grid-column-start: 2;
   grid-column-end: 3;
-  width:120px;
-  
+  width: 120px;
 `;
